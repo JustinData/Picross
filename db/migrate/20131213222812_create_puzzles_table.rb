8b5bbb048b533c4322dcb68277567ad7afc0f6a4
JustinGA <justinkemmerer@gmail.com>
2013-12-14 23:21:47 -0500

    created models, started seed file

--- db/migrate/20131213222812_create_puzzles_table.rb
+++ db/migrate/20131213222812_create_puzzles_table.rb
@@ -1,11 +1,16 @@
 class CreatePuzzlesTable < ActiveRecord::Migration
   def change
     create_table :puzzles do |t|
-    	t.string :name
-    	t.integer :x_size
-    	t.integer :y_size
+    	t.string :name, null: false, unique: true
+    	t.integer :x_size, null: false
+    	t.integer :y_size, null: false
+    end
 
-    	
+    create_table :cells do |t|
+    	t.integer :x, null: false
+    	t.integer :y, null: false
+    	t.integer :puzzle_id
+    	t.foreign_key :puzzles
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Cell.destroy_all
+Puzzle.destroy_all
+
+Puzzle.create(name: 'House', x_size: 5, y_size: 5)
+Puzzle.create(name: 'Superman', x_size: 10, y_size: 10)
+
+Cell.create(x: 2, y: 0, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 1, y: 1, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 1, y: 2, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 1, y: 3, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 2, y: 0, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 2, y: 1, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 2, y: 2, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 2, y: 3, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 2, y: 4, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 3, y: 0, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 3, y: 1, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 3, y: 3, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 3, y: 4, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 4, y: 0, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 4, y: 1, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 4, y: 3, puzzle_id: Puzzle.find_by_name('House'))
+Cell.create(x: 4, y: 4, puzzle_id: Puzzle.find_by_name('House'))
+
